9ba1c8885dea4d455de44d438901b5521f658d6f
Ashwin Muthuraman A <76959236+AshwinMuthuraman-A@users.noreply.github.com>
2023-01-10 00:34:42 +0530

    Create expression_eval.asm

---
+++ Week01/expression_eval.asm
@@ -0,0 +1,30 @@
+        .text
+        .globl main
+
+main:
+    li $v0, 4
+	  la $a0, msg1
+	  syscall
+  
+	  li $v0, 5
+	  syscall
+	  move $t0, $v0
+	
+	  add $t1, $t0, 3
+	  mul $t0, $t0, $t1
+	
+	  li $v0, 4
+	  la $a0, msg2
+	  syscall
+  
+	  li $v0, 1
+    move $a0, $t0
+	  syscall
+        
+    li $v0, 4
+    la $a0, endl
+    syscall
+        
+        .data
+msg1:   .asciiz "Enter the value of N : "
+msg2:   .asciiz "The value of i is : "
